689f475eb121cf7a5e031519ac8234b056fd1df8
me <me@gmail.com>
2019-12-13 10:55:29 -0700

    se agreaga fondo de inicio y las ventanas modales

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, NavDropdown, Nav, Table, Col, Image, Row, Form, Modal, Container, Button} from 'react-bootstrap'
+import fondo from './Chef.jpg';
+import { Navbar, NavDropdown, Nav, Table, Col, Image, Row, 
+        InputGroup, FormControl, Form, Modal, Container, Button} from 'react-bootstrap'
 
 
-function MydModalWithGrid({show, onHide}) {
+function MydModalWithGrid({show, onHide, datos}) {  
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
   return (
     <Modal show={show} onHide={onHide} aria-labelledby="contained-modal-title-vcenter">
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Using Grid in Modal
+          Identificarse
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
        <Container>
         <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" value={email} onChange={ e => setEmail(e.target.value)} />
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+              No compartas tu email y contraseña.
             </Form.Text>
           </Form.Group>
-          <Form.Group controlId="formBasicPassword">
+          <Form.Group controlId="formPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" value={pass} onChange={ e => setPass(e.target.value)}  />
           </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+          <Form.Group controlId="formRecordar">
+            <Form.Check type="checkbox" label="Recordar" />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant="primary" onClick={(e)=>{e.preventDefault(); datos(email, pass)} } >
             Submit
           </Button>
         </Form>
-
         </Container>
       </Modal.Body>
       <Modal.Footer>
@@ -43,17 +46,61 @@ function MydModalWithGrid({show, onHide}) {
   );
 }
 
+function Pay(){
+  return (
+    <div>
+      <Button variant="primary" size="lg" block style={{ marginTop: 50 }}>
+        Cobrar
+      </Button>
+ 
+    </div>
+  )
+}
+
+function Total(){
+
+  return(
+    <div>
+      <InputGroup size="lg" style={{ marginBottom:6}}>
+        <InputGroup.Prepend>
+          <InputGroup.Text id="inputGroup-sizing-lg">Sub-Total</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+      </InputGroup>
+      <InputGroup size="lg" style={{ marginBottom:6}}>
+        <InputGroup.Prepend>
+          <InputGroup.Text id="inputGroup-sizing-lg">IVA</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+      </InputGroup>
+      <InputGroup size="lg">
+        <InputGroup.Prepend>
+          <InputGroup.Text id="inputGroup-sizing-lg">Total</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+      </InputGroup>
+    </div>
+  )
+}
+
 function Display(){
   return(
     <>
-       
-        <Container fluid={true}>
+        <Container fluid={true} >
           <Row>
-            <Col sm={3}>
+            <Col sm={4}>
               <Ticket />
             </Col>
-            <Col sm={9}>
+            <Col sm={8}>
               <Articles />
+            </Col>
+          </Row>
+          <Row>
+          <Col sm={4}>
+              <Total />
+            </Col>
+            <Col sm={8}>
+              <Pay />
             </Col>
           </Row>
         </Container>
@@ -112,15 +159,22 @@ function Articles(){
 }
 function Ticket(){
   
-  const items = [...Array(20)].map((val, i) => (<tr> <td>${i}</td> <td>Chicharrones</td>    <td>1</td>     <td>$50.00</td> <td>$50.00</td> </tr>))
+  const items = [...Array(15)].map((val, i) => (
+  <tr key={i}> 
+    <td>${i}</td> 
+    <td>Chicharrones</td>    
+    <td>1</td>     
+    <td>$50.00</td> 
+    <td>$50.00</td> 
+  </tr>))
 
   
   return(
   
     <>
-      <Container style={{'max-height': 'calc(100vh - 210px)', 'overflow-y': 'auto'}}>
+      <Container style={{'maxHeight': 'calc(100vh - 300px)', 'overflowY': 'auto'}}>
         <Table striped bordered hover size="sm"  >
-          <thead fixed>
+          <thead >
             <tr>
               <th>#</th>
               <th>Articulo</th>
@@ -132,7 +186,6 @@ function Ticket(){
           <tbody>
             {
               items.map((item, i)=>(item))
-
             }
           </tbody>
         </Table>
@@ -141,14 +194,41 @@ function Ticket(){
   )
 }
 
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Logout
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Esta seguro que desea desconectarse?</h4>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Desconectarse</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+
 function NavBar(props) {
+ 
   return (
     <Navbar bg="light" expand="lg">
     <Navbar.Brand href="#home"><strong>PoS</strong></Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        <Nav.Link href="" onClick={() => props.setModalShow(true)} >Login</Nav.Link>
+        { props.login ? <Nav.Link href="" onClick={() => props.setModalShow('logout')} >Logout</Nav.Link>:
+        <Nav.Link href="" onClick={() => props.setModalShow('login')} >Login</Nav.Link>}
+        
         <NavDropdown title="Configuración" id="basic-nav-dropdown">
           <NavDropdown.Item href="#usuarios">Usuarios</NavDropdown.Item>
           <NavDropdown.Item href="#articulos">Articulos</NavDropdown.Item>
@@ -158,18 +238,108 @@ function NavBar(props) {
       </Nav>
     </Navbar.Collapse>
   </Navbar>
-      )
-    }
+    )
+  }
     
 
 function App() {
   const [modalShow, setModalShow] = useState(false);
+  const [modalShowLoggout, setModalShowLoggout] = useState(false);
+  const [compras, setCompras] = useState([])
+  const [isLoggedin, setIsloggedin] = useState(false)
+  
+  const menu = [
+    {
+      id: 0,
+      platillo: "HAMBURGUESA",
+      costo: 70,
+    },
+    {
+      id: 1,
+      platillo: "SOPA DE VERDURAS",
+      costo: 65,
+    },
+    {
+      id: 2,
+      platillo: "CARNE ASADA",
+      costo: 80,
+    },
+    {
+      id: 3,
+      platillo: "BISTEK CON VERDURAS",
+      costo: 85,
+    },
+    {
+      id: 4,
+      platillo: "ENSALADA ALL INCLUSIVE",
+      costo: 210,
+    },
+    {
+      id: 5,
+      platillo: "POLLUELO ALL INCLUSIVE",
+      costo: 220,
+    },
+    {
+      id: 6,
+      platillo: "ENCHILADAS",
+      costo: 65,
+    },
+    {
+      id: 7,
+      platillo: "BISTEZOTE",
+      costo: 90,
+    },
+    {
+      id: 8,
+      platillo: "MEXCOLANZA",
+      costo: 95,
+    },
+    {
+      id: 9,
+      platillo: "WORM-BURGUER",
+      costo: 100,
+    },
+    {
+      id: 10,
+      platillo: "SOPA DE CAMARON",
+      costo: 120,
+    },
+    {
+      id: 11,
+      platillo: "CAMARONIZA",
+      costo: 80,
+    },    
+  ]
+   
 
+  function handleLogin(email, pass){
+    if(email === 'oscaralberto@gmail.com' && pass === '123456'){
+      setIsloggedin(true)
+      setModalShow(false)
+
+    }else{
+      setIsloggedin(false)
+    }
+  }
+
+  function status(isLoggout){
+    if (isLoggout) setIsloggedin(false)
+  }
+  function showModal(modal){
+    modal === 'login' ? setModalShow(true) : setModalShowLoggout(true)
+  }
   return (
       <>
-        <NavBar setModalShow={setModalShow} />
-        <Display />
-        <MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
+          <NavBar setModalShow={showModal} login={isLoggedin} status={status} />
+          <MydModalWithGrid show={modalShow} datos={handleLogin} onHide={() => setModalShow(false)} />
+          <MyVerticallyCenteredModal
+          show={modalShowLoggout}
+          onHide={() => {setModalShowLoggout(false); setIsloggedin(false)} }
+        />
+          { isLoggedin ? <Display /> : <Image src="img/p1.png" className="rounded mx-auto d-block" />}
+
+          {/* { isLoggedin && <Display />} */}
+
       </>
       );
     }
